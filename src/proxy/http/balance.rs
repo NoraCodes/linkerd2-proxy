8c53c92d540f09338648cfcb7e161b5e53611638
Oliver Gould <ver@buoyant.io>
2019-03-31 20:10:02 +0000

    wire weights through from the destination service to the balancer

--- src/proxy/http/balance.rs
+++ src/proxy/http/balance.rs
@@ -9,7 +9,9 @@ use std::marker::PhantomData;
 use std::time::Duration;
 
 pub use self::hyper_balance::{PendingUntilFirstData, PendingUntilFirstDataBody};
-pub use self::tower_balance::{choose::PowerOfTwoChoices, load::WithPeakEwma, Balance};
+pub use self::tower_balance::{
+    choose::PowerOfTwoChoices, load::WithPeakEwma, Balance, HasWeight, Weight, WithWeighted,
+};
 
 use http;
 use svc;
@@ -59,7 +61,8 @@ impl<T, M, A, B> svc::Layer<T, T, M> for Layer<A, B>
 where
     M: svc::Stack<T> + Clone,
     M::Value: Discover,
-    <M::Value as Discover>::Service: svc::Service<http::Request<A>, Response = http::Response<B>>,
+    <M::Value as Discover>::Service:
+        HasWeight + svc::Service<http::Request<A>, Response = http::Response<B>>,
     A: Payload,
     B: Payload,
     Error: From<<M::Value as Discover>::Error>
@@ -96,19 +99,66 @@ impl<T, M, A, B> svc::Stack<T> for Stack<M, A, B>
 where
     M: svc::Stack<T> + Clone,
     M::Value: Discover,
-    <M::Value as Discover>::Service: svc::Service<http::Request<A>, Response = http::Response<B>>,
+    <M::Value as Discover>::Service:
+        HasWeight + svc::Service<http::Request<A>, Response = http::Response<B>>,
     A: Payload,
     B: Payload,
     Error: From<<M::Value as Discover>::Error>
         + From<<<M::Value as Discover>::Service as svc::Service<http::Request<A>>>::Error>,
 {
-    type Value = Balance<WithPeakEwma<M::Value, PendingUntilFirstData>, PowerOfTwoChoices>;
+    type Value =
+        Balance<WithWeighted<WithPeakEwma<M::Value, PendingUntilFirstData>>, PowerOfTwoChoices>;
     type Error = M::Error;
 
     fn make(&self, target: &T) -> Result<Self::Value, Self::Error> {
         let discover = self.inner.make(target)?;
         let instrument = PendingUntilFirstData::default();
-        let loaded = WithPeakEwma::new(discover, self.default_rtt, self.decay, instrument);
-        Ok(Balance::p2c(loaded))
+        let load = WithPeakEwma::new(discover, self.default_rtt, self.decay, instrument);
+        Ok(Balance::p2c(WithWeighted::from(load)))
+    }
+}
+
+pub mod weight {
+    use super::tower_balance::{HasWeight, Weighted};
+    use svc;
+
+    #[derive(Clone, Debug)]
+    pub struct Layer(());
+
+    #[derive(Clone, Debug)]
+    pub struct Stack<M> {
+        inner: M,
+    }
+
+    pub fn layer() -> Layer {
+        Layer(())
+    }
+
+    impl<T, M> svc::Layer<T, T, M> for Layer
+    where
+        M: svc::Stack<T>,
+        T: HasWeight,
+    {
+        type Value = <Stack<M> as svc::Stack<T>>::Value;
+        type Error = <Stack<M> as svc::Stack<T>>::Error;
+        type Stack = Stack<M>;
+
+        fn bind(&self, inner: M) -> Self::Stack {
+            Stack { inner }
+        }
+    }
+
+    impl<T, M> svc::Stack<T> for Stack<M>
+    where
+        M: svc::Stack<T>,
+        T: HasWeight,
+    {
+        type Value = Weighted<M::Value>;
+        type Error = M::Error;
+
+        fn make(&self, target: &T) -> Result<Self::Value, Self::Error> {
+            let inner = self.inner.make(&target)?;
+            Ok(Weighted::new(inner, target.weight()))
+        }
     }
 }
